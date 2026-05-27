316f6f72d5fe2b4ad6f79ed52bb22f7ceefde9c2
Hoang <hmy@compute1-client-1.ris.wustl.edu>
2026-05-27 02:09:22 -0500

    clean up

--- splice2neo/didntWork/v0.6.13_gencodemm10_ens95/build_image.sh
+++
@@ -1,2 +0,0 @@
-cd $(dirname $0)
-bsub -G compute-oncology -q oncology-interactive -Is -a 'docker_build(myhoang04/splice2neo:v0.6.13_gencodemm10_ens95)' -- --tag myhoang04/splice2neo:v0.6.13_gencodemm10_ens95 .
